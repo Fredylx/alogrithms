727a5615d9d68fa643a7fc4f7db4e584b761a715
Fred Lopez <84998401+Fredylx@users.noreply.github.com>
2022-10-11 21:10:35 -0700

    Create recursive_binary_search.py

---
+++ recursive_binary_search.py
@@ -0,0 +1,33 @@
+'''
+Python 3
+Fredy H Lopez
+10/11/2022
+Youtube: Algorithms and Data Structures Tutorial - Full Course for Beginners
+'''
+
+def recursive_binary_search(list, target):
+    if len(list) == 0:
+        return False
+    else:
+        midpoint = (len(list))//2
+            
+        if list[midpoint] == target:
+            return True
+        else:
+            if list[midpoint] < target:
+                return recursive_binary_search(list[midpoint + 1:], target)
+            else:
+                return recursive_binary_search(list[:midpoint], target)
+            
+def verify(result):
+    print("target found: ", result)
+    
+numbers = [1,2,3,4,5,6,7,8]
+result = recursive_binary_search(numbers, 12)
+verify(result)
+
+result = recursive_binary_search(numbers, 6)
+verify(result)
+
+            
+        
